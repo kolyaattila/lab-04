7fc476d1bee55ef89acfd91eed8828d15fd78480
Attila Kolya, INI-DOS-DE-TEO (EXT) <attila.kolya@swisscom.com>
2022-10-26 15:40:23 +0300

    second commit

---
+++ src/Circle.java
@@ -0,0 +1,41 @@
+public class Circle {
+	private int radius, x, y;
+
+	public Circle() {
+		radius = 10;
+		x = 20;
+		y = 20;
+	}
+
+	public Circle(int r, int a, int b) {
+		radius = r;
+		x = a;
+		y = b;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int r) {
+		radius = r;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int a) {
+		x = a;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int b) {
+		y = b;
+	}
+}
+
+
